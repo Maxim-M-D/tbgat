0cc81b808d509253b694a81ddbb09afaa68ca907
Alexander Marquard <alexandermarquard@kpmg.com>
2024-08-17 11:25:45 +0200

    fix also other imports

--- tbgat/location_mapping/OpenStreetMapLocationMapper.py
+++ tbgat/location_mapping/OpenStreetMapLocationMapper.py
@@ -18,6 +18,11 @@ import shapely.geometry
 import logging
 
 from tenacity import retry, stop_after_attempt, wait_fixed
+import pkg_resources
+
+get_file_path = lambda x: pkg_resources.resource_filename(
+    "tbgat", f"location_mapping/data/{x}"
+)
 
 logger = logging.getLogger(__name__)
 
@@ -36,8 +41,9 @@ class OpenStreetMapLocationMapper:
 
     def _read_adm1_geojson(self):
         """Reads the geojson file containing the ADM1 boundaries of Ukraine and merges Kyiv and Kyiv Oblast into one polygon."""
+        path = get_file_path("geoBoundaries-UKR-ADM1.geojson")
         self.adm1: gpd.GeoDataFrame = gpd.read_file(
-            "./tbgat/location_mapping/data/geoBoundaries-UKR-ADM1.geojson"
+            path
         )
         kyiv_oblast = self.adm1[self.adm1["shapeName"] == "Kyiv Oblast"]
         kyiv = self.adm1[self.adm1["shapeName"] == "Kyiv"]
